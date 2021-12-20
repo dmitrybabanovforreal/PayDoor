bfcf1c2a55d783fa335bfd0d1b99a5173df86290
dmitrybabanovforreal <babanov.d.e@gmail.com>
2021-12-19 20:06:52 -0500

    Initial commit

---
+++ paydoor-front/src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import '../tailwind.css'
+</script>
+
+<nav class="bg-white">
+    <div class="shadow-lg mx-6">
+    <span>PayDoor</span>
+    <span><a>Join the waitlist</a></span>
+    </div>
+</nav>
+
+<slot/>
---
+++ paydoor-front/src/routes/index.svelte
@@ -0,0 +1,49 @@
+<div>
+    <p>One subscription to get behind (almost) any paywall</p>
+</div>
+
+<!--almost full screen section-->
+
+<!--large heading-->
+<div>
+    <p>One subscription to get behind (almost) any paywall</p>
+    <!--button-->
+    <p>Join the waitlist</p>
+</div>
+
+<div>
+    <p>Hit another paywall on an article you saw on Facebook. They demand subscription to read it although you'll
+        probably never visit this local news site again. But what can they do, they wanna make money too.</p>
+    <p>Wouldn't it be cool to pay them once to read this article only? With one click? No forms, no email
+        verification?</p>
+</div>
+
+<div>
+    <p>With PayDoor, you only need one subscription. You open an article, see the paywall, click "Open with
+        PayDoor" and read that article only. Payment is sent to them from your PayDoor account.</p>
+</div>
+
+<div>
+<!--an image with the paywall and that button-->
+</div>
+
+<div>
+    <p>Support local news outlets. People tend to subscribe to large news websites that cover a broad range of
+        topics. With PayDoor, a local news website has a chance to get paid for their hard work too when they
+        publish a viral story.</p>
+</div>
+
+<div>
+    <p>Current progress step: explaining this thing to the first publishers</p>
+    <p>Join the waitlist though. You will only get an email when it's launched or if there is a *really*
+        important update on the progress.</p>
+    <!--email field-->
+    <!--button-->
+    <p>Join the waitlist</p>
+</div>
+
+<div>
+    <!--footer-->
+    <p>2021 Copy rights reserved. All that stuff, you know</p>
+    <p>How to reach out to me:</p>
+</div>
