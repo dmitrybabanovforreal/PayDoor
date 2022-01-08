5ed81699d98bb7c8733461ae50b9d66dd8329093
dmitrybabanovforreal <babanov.d.e@gmail.com>
2022-01-07 20:47:11 -0500

    Updates in the interface

--- paydoor-front/src/routes/__layout.svelte
+++ paydoor-front/src/routes/__layout.svelte
@@ -2,11 +2,12 @@
     import '../tailwind.css'
 </script>
 
-<nav class="bg-white">
-    <div class="shadow-lg mx-6">
-    <span>PayDoor</span>
-    <span><a>Join the waitlist</a></span>
+
+<header class="fixed w-full bg-white py-1.5 px-3 shadow-lg">
+    <div class="flex flex-row justify-between items-center">
+        <div class="m-1 text-blue-700 font-bold text-2xl">PayDoor</div>
+        <div class="m-1 px-3 py-1 text-gray-500 text-sm bg-gray-100 hover:bg-gray-200 rounded-full"><a>Join the waitlist</a></div>
     </div>
-</nav>
+</header>
 
 <slot/>
--- paydoor-front/src/routes/index.svelte
+++ paydoor-front/src/routes/index.svelte
@@ -1,49 +1,62 @@
-<div>
-    <p>One subscription to get behind (almost) any paywall</p>
+<div class="bg-blue-400">
+    <div class="py-28 mx-5 flex flex-col justify-center items-center">
+        <h1 class="m-3 text-5xl font-bold text-white text-center">PayDoor</h1>
+        <h2 class="m-3 text-xl font-bold text-white text-center">
+            One subscription to get behind (almost) any paywall
+        </h2>
+        <button class="py-2 px-4 m-3 bg-gray-100 hover:bg-gray-200 rounded-md font-bold shadow-lg">
+            Join the waitlist
+        </button>
+    </div>
 </div>
 
-<!--almost full screen section-->
+<p class="m-8">Imagine: you clicked an article somewhere, and there is a paywall.
+    You'll probably never visit this website again. But they need to make money, right?</p>
 
-<!--large heading-->
-<div>
-    <p>One subscription to get behind (almost) any paywall</p>
-    <!--button-->
-    <p>Join the waitlist</p>
+<div class="m-10">
+    <img class="shadow-2xl rounded-md" src="static/vectorstock_35145930.png" alt="How a paywall with PayDoor button looks">
 </div>
 
-<div>
-    <p>Hit another paywall on an article you saw on Facebook. They demand subscription to read it although you'll
-        probably never visit this local news site again. But what can they do, they wanna make money too.</p>
-    <p>Wouldn't it be cool to pay them once to read this article only? With one click? No forms, no email
-        verification?</p>
+
+<p class="m-8">Wouldn't it be cool to pay them let's say $0.25 to read this
+    article only? With one click? No sign up forms, no email verification?</p>
+<p class="m-8">With PayDoor, you only need one subscription to get behind any
+    paywall. You open an article, see the paywall, click <b>"Open with PayDoor"</b> and
+    read that article only. The payment is sent to them from your PayDoor account.</p>
+
+<div class="m-10">
+    <img class="shadow-2xl rounded-md" src="static/mockup.png" alt="How a paywall with PayDoor button looks">
 </div>
 
-<div>
-    <p>With PayDoor, you only need one subscription. You open an article, see the paywall, click "Open with
-        PayDoor" and read that article only. Payment is sent to them from your PayDoor account.</p>
+<p class="m-8">Support local news outlets! People tend to subscribe to large news websites
+    that already cover a broad range of topics.</p>
+<p class="m-8">With PayDoor, a local news website has a chance to get paid too. Because if
+    they publish a viral story and suddenly receive a ton of random visitors, those could
+    pay them for their hard work with just one click and enjoy the piece.</p>
+
+<div class="m-10">
+    <img class="shadow-2xl rounded-md" src="static/vectorstock_20886334.png" alt="How a paywall with PayDoor button looks">
 </div>
 
-<div>
-<!--an image with the paywall and that button-->
-</div>
+<p class="m-8">When you out of town and you come to a restaurant you see the first time
+    to have a lunch, do they ask you to sign up for a monthly subscription to eat there?
+    No, you hand over your credit card and your bank account is charged with the
+    bill amount.</p>
+<p class="m-8">It has to be equally easy with websites. If there is a paywall,
+    there should be PayDoor.</p>
 
-<div>
-    <p>Support local news outlets. People tend to subscribe to large news websites that cover a broad range of
-        topics. With PayDoor, a local news website has a chance to get paid for their hard work too when they
-        publish a viral story.</p>
-</div>
-
-<div>
-    <p>Current progress step: explaining this thing to the first publishers</p>
-    <p>Join the waitlist though. You will only get an email when it's launched or if there is a *really*
-        important update on the progress.</p>
+<div class="py-10 px-6 flex flex-col justify-center items-center bg-gray-100">
+    <p class="m-6">Current progress step: explaining this thing to the first publishers</p>
+    <p class="m-6">Join the waitlist though. You will only get an email when it's launched or
+        if there is a *really* important update on the progress.</p>
     <!--email field-->
-    <!--button-->
-    <p>Join the waitlist</p>
+    <button class="py-2 px-4 m-3 bg-blue-400 hover:bg-blue-500 rounded-md justify-self-center self-center font-bold text-white shadow-lg">
+        Join the waitlist
+    </button>
 </div>
 
-<div>
+<div class="p-3 bg-gray-700 text-gray-300 text-sm">
     <!--footer-->
-    <p>2021 Copy rights reserved. All that stuff, you know</p>
-    <p>How to reach out to me:</p>
+    <p class="">2021 Copy rights reserved. All that stuff, you know</p>
+    <p class="">How to reach out to me:</p>
 </div>
